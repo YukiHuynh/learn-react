4c1738ec8acaf8301ccf2ef6227bf03a036ebe14
Vince <hvhn3295@gmail.com>
2024-10-21 17:53:20 +0700

    react tic-tac-toe app continue 21.10.2024

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -9,11 +9,20 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquare] = useState(Array(9).fill(null));
   function handleClick(i) {
+    if(squares[i]) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if(xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "0";
+    }
     setSquare(nextSquares);
+    setXIsNext(!xIsNext);
   }
   return (
   <>
@@ -37,4 +46,24 @@ export default function Board() {
 
   </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for(let i = 0; i < lines.length; i++) {
+    cont [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
